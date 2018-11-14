6c11ded387e0b612b17c1d8d1f79700df732f84f
Daniel Pak <PakJuhnMan@gmail.com>
2018-11-14 13:24:44 -0700

    added in javascript and styling changes for hover over service plans

---
+++ public/js/serviceplans.js
@@ -0,0 +1,47 @@
+//service plan 1 on hover - mouseover & mouseout
+document.getElementById('service-plan-1').addEventListener('mouseover', function() {
+    console.log('what what?!');
+    document.getElementById('service-plan-1').style.backgroundColor = '#57C8FF';
+})
+
+document.getElementById('service-plan-1').addEventListener('mouseout', function() {
+    console.log('bye bye! have a good time!');
+    document.getElementById('service-plan-1').style.backgroundColor = '#2A9CC2';
+
+})
+
+//service plan 2 on hover
+document.getElementById('service-plan-2').addEventListener('mouseover', function() {
+    console.log('what what?!');
+    document.getElementById('service-plan-2').style.backgroundColor = '#78FFC9';
+})
+
+document.getElementById('service-plan-2').addEventListener('mouseout', function() {
+    console.log('bye bye! have a good time!');
+    document.getElementById('service-plan-2').style.backgroundColor = '#4BA07E';
+
+})
+
+//service plan 3 on hover
+document.getElementById('service-plan-3').addEventListener('mouseover', function() {
+    console.log('what what?!');
+    document.getElementById('service-plan-3').style.backgroundColor = '#DFFF72';
+})
+
+document.getElementById('service-plan-3').addEventListener('mouseout', function() {
+    console.log('bye bye! have a good time!');
+    document.getElementById('service-plan-3').style.backgroundColor = '#CBE80C';
+
+})
+
+//service plan 4 on hover
+document.getElementById('service-plan-4').addEventListener('mouseover', function() {
+    console.log('what what?!');
+    document.getElementById('service-plan-4').style.backgroundColor = '#FF00B2';
+})
+
+document.getElementById('service-plan-4').addEventListener('mouseout', function() {
+    console.log('bye bye! have a good time!');
+    document.getElementById('service-plan-4').style.backgroundColor = '#D10092';
+
+})
