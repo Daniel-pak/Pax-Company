97ed459b1ed0d0982af0885bd2c38aaae582b497
Daniel Pak <PakJuhnMan@gmail.com>
2018-11-19 13:49:27 -0700

    adding in path variable for admin - added in psuedocode logic for controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,12 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin/{path}", name="admin")
      */
-    public function index()
+    public function index($path)
     {
+        //token authentication
+          //if token is set -> check against DB
+            //if return is true, redirect to path -> admin/home
+          //else return
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'path' => $path
         ]);
     }
 }
